ac993a87a6b18518f4c5afa40dd7e2db63eb6f2a
Anthony Sinitsa <itonyemail@gmail.com>
2025-08-21 23:49:42 -0700

    Initial commit of my neovim config

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,7 @@
+-- Center the cursor when scrolling half a page
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
+
+-- Center the cursor when jumping to the next/previous search result
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result and center' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result and center' })
